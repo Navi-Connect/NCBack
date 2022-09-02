b60dd4791c9abe45a4283480be177d1627c2d5c6
makabake9 <bauyrzhan.maksot@nu.edu.kz>
2022-09-02 18:52:07 +0600

    restart service

--- NCBack/Services/UploadFileService.cs
+++ NCBack/Services/UploadFileService.cs
@@ -6,6 +6,7 @@ public class UploadFileService
 {
     public async void Upload(string path, string fileName, IFormFile file)
     {
+        //
         await using var stream = new FileStream(Path.Combine(path, fileName), FileMode.Create);
         await file.CopyToAsync(stream);
     }
