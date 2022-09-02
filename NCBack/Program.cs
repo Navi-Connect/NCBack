e639ee6c32479d85a99350e2949f14ddbce5ff9a
Dosgali  Iskander <idosgali@gmail.com>
2022-09-02 15:35:36 +0600

    123

--- NCBack/Program.cs
+++ NCBack/Program.cs
@@ -59,14 +59,14 @@ builder.Services.AddScoped<IFightService, FightService>();*/
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
