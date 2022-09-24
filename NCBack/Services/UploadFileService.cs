88b1e0e80cb000927b38af405475db7b2e0c25e8
Dosgali  Iskander <idosgali@gmail.com>
2022-09-24 20:53:41 +0600

    photo

--- NCBack/Services/UploadFileService.cs
+++ NCBack/Services/UploadFileService.cs
@@ -4,10 +4,7 @@ public class UploadFileService
 {
     public async void Upload(string path, string fileName, IFormFile file)
     {
-        Console.WriteLine(path);
-        Console.WriteLine(fileName);
-        Console.WriteLine(Path.Combine(path, fileName));
-        var stream = File.Create("fsa.txt");
+        using var stream = new FileStream(Path.Combine(path, fileName), FileMode.Create);
         await file.CopyToAsync(stream);
     }
 }
