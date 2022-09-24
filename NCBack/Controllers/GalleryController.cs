5e69630888621cd85f1d847bd1dbdff70659c6f5
Dosgali  Iskander <idosgali@gmail.com>
2022-09-24 21:29:35 +0600

    controller

--- NCBack/Controllers/GalleryController.cs
+++ NCBack/Controllers/GalleryController.cs
@@ -2,7 +2,7 @@
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace NCBack.Controllers;
-[Route("api/[controller]")]
+
 public class GalleryController : Controller
 {
     private readonly IHostingEnvironment _hostingEnvironment;
@@ -13,10 +13,10 @@ public class GalleryController : Controller
     }        
 
     // GET api/<controller>/5
-    [HttpGet("{id}")]
+    [HttpGet("images/{id}")]
     public IActionResult Get(string id)
     {
-        var path = Path.Combine(_hostingEnvironment.WebRootPath, "images", $"{id}.jpg");
+        var path = Path.Combine(_hostingEnvironment.WebRootPath, "images", $"{id}");
         var imageFileStream = System.IO.File.OpenRead(path);
         return File(imageFileStream, "image/jpeg");
     }
