ddff34b5f0aaffb32229637c3dbae0840778ebb0
Dosgali  Iskander <idosgali@gmail.com>
2022-10-10 11:59:55 +0600

    Push

--- NCBack/Services/EmailService.cs
+++ NCBack/Services/EmailService.cs
@@ -1,6 +1,12 @@
 ﻿using MailKit.Net.Smtp;
 using MimeKit;
 
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+using MailKit.Security;
+
 namespace NCBack.Services;
 
 public class EmailService
@@ -9,14 +15,14 @@ public class EmailService
     {
         var emailMessage = new MimeMessage();
         emailMessage.From.Add(new MailboxAddress("Приложения Navi Connect",
-            "busindavis@yandex.kz"));
+            "crystel97@ethereal.email"));
         emailMessage.To.Add(new MailboxAddress("", email));
         emailMessage.Subject = subject;
         emailMessage.Body = new BodyBuilder() { HtmlBody = message }.ToMessageBody();
         emailMessage.Prepare(EncodingConstraint.EightBit);
         using var client = new SmtpClient();
-        await client.ConnectAsync("smtp.yandex.ru", 25, false);
-        await client.AuthenticateAsync("busindavis@yandex.kz", "rindoman666");
+        await client.ConnectAsync("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync("crystel97@ethereal.email", "E3HSv6pm85nkkB7jYc");
         await client.SendAsync(emailMessage);
         await client.DisconnectAsync(true);
     }
