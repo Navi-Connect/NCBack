42768930bc5d8ec61c95a33d90869f8d878575ce
Dosgali  Iskander <idosgali@gmail.com>
2022-10-11 13:22:13 +0600

    Commit

--- NCBack/Services/EmailService.cs
+++ NCBack/Services/EmailService.cs
@@ -1,11 +1,5 @@
-﻿using MailKit.Net.Smtp;
-using MimeKit;
-
-using SendGrid;
+﻿using SendGrid;
 using SendGrid.Helpers.Mail;
-using System;
-using System.Threading.Tasks;
-using MailKit.Security;
 
 namespace NCBack.Services;
 
@@ -13,11 +7,10 @@ public class EmailService
 {
     public static async Task SendMessageAsync(string email, string subject, string message)
     {
-        
-        var apiKey = "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";
+        var apiKey = " ";
         var client = new SendGridClient(apiKey);
-        var from = new EmailAddress("SupNaviConnect@gmail.com", "Navi Connect");
-        var to = new EmailAddress(email, " ");
+        var from = new EmailAddress("supnaviconnect@gmail.com", "Navi");
+        var to = new EmailAddress(email);
         var plainTextContent = "С уважением Navi Connect";
         var htmlContent = message;
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
