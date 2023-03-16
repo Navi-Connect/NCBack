47f264305e548ec14e910afa064cae15bd3c87e3
iskander1234 <64252756+iskander1234@users.noreply.github.com>
2023-03-16 11:16:16 +0600

    Добавил в логин поля

--- NCBack/Dtos/User/TokenDto.cs
+++ NCBack/Dtos/User/TokenDto.cs
@@ -1,7 +1,22 @@
-﻿namespace NCBack.Dtos.User;
+﻿using NCBack.Models;
+
+namespace NCBack.Dtos.User;
 
 public class TokenDto
 {
+    public int UserId { get; set; }
+    public int? CityId { get; set; }
+    public CityList? City { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string? FullName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string AvatarPath { get; set; }
+    public int? GenderId { get; set; }
+    public GenderList? Gender { get; set; }
     public string AccessToken { get; set; }
     public string RefreshToken { get; set; }
+    public string? DeviceId { get; set; }
+    public bool Success { get; set; } = true;
+    public string Message { get; set; } = string.Empty;
 }
